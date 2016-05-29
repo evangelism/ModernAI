ff65c39a315a3393eb239cfb1ec030410fc26730
Dmitri Soshnikov <dmitri@soshnikov.com>
2016-05-29 16:25:56 +0300

    Add DialogBotComposition

---
+++ DialogBotComposition/DialogBot/Controllers/MessagesController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Microsoft.Bot.Connector;
+using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs;
+using MSEvangelism.OpenWeatherMap;
+using System.Text;
+
+namespace DialogBot
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<Message> Post([FromBody]Message message)
+        {
+
+            if (message.Type == "Message")
+            {
+                // return our reply to the user
+                return await Conversation.SendAsync(message, () =>
+                  new WeatherDialog()
+                    .ContinueWith<WeatherParam, string>(async (ctx, wpa) =>
+                     {
+                         var WP = await wpa;
+                         return new ChoiceDialog($"Do you want to subscribe to weather in {WP.Location}", new string[] { "Yes", "No" });
+                     }));
+            }
+            else
+            {
+                return HandleSystemMessage(message);
+            }
+        }
+
+        private Message HandleSystemMessage(Message message)
+        {
+            if (message.Type == "Ping")
+            {
+                Message reply = message.CreateReplyMessage();
+                reply.Type = "Ping";
+                return reply;
+            }
+            else if (message.Type == "DeleteUserData")
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == "BotAddedToConversation")
+            {
+            }
+            else if (message.Type == "BotRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "UserAddedToConversation")
+            {
+            }
+            else if (message.Type == "UserRemovedFromConversation")
+            {
+            }
+            else if (message.Type == "EndOfConversation")
+            {
+            }
+
+            return null;
+        }
+    }
+}
---
+++ DialogBotComposition/DialogBot/StringDialog.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DialogBot
+{
+    [Serializable]
+    public class StringDialog : IDialog<string>
+    {
+        protected string msg;
+        public StringDialog(string msg)
+        {
+            this.msg = msg;
+        }
+        public async Task StartAsync(IDialogContext context)
+        {
+            await context.PostAsync(msg);
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<Message> argument)
+        {
+            var message = await argument;
+            context.Done(message.Text);
+        }
+
+    }
+}
