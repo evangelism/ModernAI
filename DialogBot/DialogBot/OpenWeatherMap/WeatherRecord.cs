33900d705572b2671d9b2f2ab5317b36bbd9261e
Dmitri Soshnikov <dmitri@soshnikov.com>
2016-05-29 13:50:13 +0300

    Add DialogBot - helloworld dialog implementation

---
+++ DialogBot/DialogBot/OpenWeatherMap/WeatherRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSEvangelism.OpenWeatherMap
+{
+    public class WeatherRecord
+    {
+        public double Temp { get; set; }
+        public double Pressure { get; set; }
+        public int Humidity { get; set; }
+        public DateTime When { get; set; }
+        public string Date
+        {
+            get { return $"{When.Day:D2}.{When.Month:D2}"; }
+        }
+
+        public string FullDate
+        {
+            get { return When.ToString("D"); }
+        }
+
+    }
+}
---
+++ DialogBot/DialogBot/WeatherDialog.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DialogBot
+{
+    public class WeatherDialog : IDialog<WeatherParam>
+    {
+
+        WeatherParam WP = new WeatherParam();
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<Message> argument)
+        {
+            var message = await argument;
+            await context.PostAsync("You said: " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+
+
+    }
+}
